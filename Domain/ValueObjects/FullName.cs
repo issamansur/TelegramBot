79f4142f6f7c061edb1b9950381a4d2300b6a4d3
Issa <issa_mansur@mail.ru>
2024-04-23 22:39:52 +0300

    Fix

--- Domain/ValueObjects/FullName.cs
+++ Domain/ValueObjects/FullName.cs
@@ -1,12 +1,12 @@
 namespace Domain.ValueObjects;
 
-public class FullName
+public class Fullname
 {
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public string? MiddleName { get; set; }
     
-    public FullName(string firstName, string lastName, string? middleName)
+    public Fullname(string firstName, string lastName, string? middleName)
     {
         FirstName = firstName;
         LastName = lastName;
